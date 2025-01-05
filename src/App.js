547833df4d5a017daebd88c798b701bdaee6fdb4
Mithil Mistry <mithil20056mistry@gmail.com>
2025-01-05 13:34:02 +0530

    Switched to HashRouter for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
@@ -79,7 +79,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename="/portfolio">
+    <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
